80609f0b638a8163d6d4c483809fc4c95f5fceb6
gorohash <hayato-shimoji@cybozu.co.jp>
2026-04-23 22:00:36 +0900

    init.sh のクォーティング修正とディレクトリシンボリックリンクの安全性を改善

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-basedir=$(cd $(dirname $0); pwd)
+basedir="$(cd "$(dirname "$0")" && pwd)"
 
-ln -sf ${basedir}/.tigrc ~/.tigrc
-ln -sf ${basedir}/.vimrc ~/.vimrc
-ln -sf ${basedir}/.zshrc ~/.zshrc
-ln -sf ${basedir}/.vim ~/.vim
+ln -sf "${basedir}/.tigrc" ~/.tigrc
+ln -sf "${basedir}/.vimrc" ~/.vimrc
+ln -sf "${basedir}/.zshrc" ~/.zshrc
+ln -sfn "${basedir}/.vim" ~/.vim
